a65a3ad285ea80c09ab34060ad3ec0479276036d
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-04-09 10:31:26 +0430

    Update pygcpp-android.sh

--- pygcpp-android.sh
+++ pygcpp-android.sh
@@ -1,8 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
 pkg update && pkg upgrade -y
 pkg install python clang python-pip git openssl -y
 git clone https://github.com/LostRuins/koboldcpp && cd koboldcpp
 make
+echo -e "Downloading the Pygmalion 6B model ... "
 wget https://huggingface.co/alpindale/pygmalion-6b-ggml/resolve/main/pygmalion-6b-v3-q4_0.bin
 python koboldcpp.py pygmalion-6b-v3-q4_0.bin
